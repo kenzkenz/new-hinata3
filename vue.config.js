30d3b0a898c24800dfc3fa745670971d84628273
kenotiai <kenotiai@gmail.com>
2018-12-04 20:00:47 +0900

    first-commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //  ビルドしたときのパス 空文字にして相対パスにする。
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? ''
+    : '',
+  // vue-awesomeの設定。最適化される？
+  transpileDependencies: [
+    /\bvue-awesome\b/
+  ],
+  // performance: {
+  //     // デフォルトは244 KiB. 10メガに変更
+  //   maxEntrypointSize: 10000000,
+  //   maxAssetSize: 10000000
+  // }
+}
